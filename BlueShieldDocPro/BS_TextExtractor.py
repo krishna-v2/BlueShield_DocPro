4caceb7015ee7fe8be0ed6d3c16d2ec6fbe797d0
Deepti Rajput <dr@apexai.us>
2022-05-20 19:29:49 -0700

    Fixed phone numbers for parenthesis and duplicate

--- BlueShieldDocPro/BS_TextExtractor.py
+++ BlueShieldDocPro/BS_TextExtractor.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 from enum import Enum
 from google.cloud import vision
 from typing import Tuple
+from cnfg import Cnfg
+import tensorflow as tf
+import numpy as np
 
 
 # In[3]:
@@ -22,141 +25,130 @@ class FeatureType(Enum):
     SYMBOL = 5
 
 
-class FindText():
+def find_text(image_path):
+    # calling up google vision json file
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'neutron-345122-39e5256f7765.json'
 
-    def __init__(self, image_path):
+    # initiate a client
+    client = vision.ImageAnnotatorClient()
 
-        # calling up google vision json file
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
-   
-        # initiate a client
-        client = vision.ImageAnnotatorClient()
+    # load image into memory
+    with io.open(image_path, 'rb') as image_file:
+        file_content = image_file.read()
 
-        # load image into memory
-        with io.open(image_path, 'rb') as image_file:
-            file_content = image_file.read()
+    # perform text detection from the image
+    image_detail = vision.Image(content=file_content)
+    response = client.document_text_detection(image=image_detail, image_context={"language_hints": ["en"]})
+    document = response.full_text_annotation
 
-        # perform text detection from the image
-        image_detail = vision.Image(content=file_content)
-        response = client.document_text_detection(image=image_detail, image_context={"language_hints": ["en"]})
-        document = response.full_text_annotation
-        
-        """Returns document bounds given an image."""
-        bounds=[]
-        feature = FeatureType.BLOCK
-        
-        for page in document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            if feature == FeatureType.SYMBOL:
-                                bounds.append(symbol.bounding_box)
+    return extract_text_helper(document)
 
-                        if feature == FeatureType.WORD:
-                            bounds.append(word.bounding_box)
 
-                    if feature == FeatureType.PARA:
-                        bounds.append(paragraph.bounding_box)
+def intersect(tl, tr, br, bl, canvs_pos):
+    xs = [tl.x, tr.x, br.x, bl.x]
+    ys = [tl.y, tl.y, br.y, bl.y]
 
-                if feature == FeatureType.BLOCK:
-                    bounds.append(block.bounding_box)
+    lw = min(xs) # left word found by vision
+    rw = max(xs)
+    tw = min(ys)
+    bw = max(ys)
 
-        # The list 'bounds' contains the coordinates of the bounding boxes.
-        self.bounds = bounds
-        self.document = document
+    lc = canvs_pos['canvas_w_offset'] # left of box in canvas
+    rc = lc + canvs_pos['we'] - canvs_pos['ws']
+    tc = canvs_pos['canvas_h_offset']
+    bc = tc + canvs_pos['he'] - canvs_pos['hs']
 
-    @staticmethod
-    def vertices_to_box(v1, v2, v3, v4) -> Tuple: #[left, right, bot, top]
-        xs = [i.x for i in [v1, v2, v3, v4]]
-        ys = [i.y for i in [v1, v2, v3, v4]]
-        left = min(xs)
-        right = max(xs)
-        bot = min(ys)
-        top = max(ys)
-        return left, right, bot, top
+    # return 0 if there is no overlap
+    if lw == rw or tw == bw:
+        return 0
+    if lw > rc or lc > rw:
+        return 0
+    if tw > bc or tc > bw:
+        return 0
 
-    def extract_address(self, flag=0):
-        bounds = self.bounds
-        ID = ''
-        name = ''
-        address = ''
-        data = {}
-        for page in self.document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            min_x, max_x, min_y, max_y = self.vertices_to_box(symbol.bounding_box.vertices[0],
-                                                                              symbol.bounding_box.vertices[1],
-                                                                              symbol.bounding_box.vertices[2],
-                                                                              symbol.bounding_box.vertices[3])
-                            
-                            if(min_x >= bounds[flag].vertices[0].x-20 and max_x <= bounds[flag].vertices[2].x+20 and min_y >= bounds[flag].vertices[0].y-10 and max_y <= bounds[flag].vertices[0].y+40):
-                                ID += symbol.text
-                            data['ID'] = ID
-                            
-                            if(min_x >= bounds[flag].vertices[0].x-20 and max_x <= bounds[flag].vertices[2].x+20 and min_y >= bounds[flag].vertices[0].y+30 and max_y <= bounds[flag].vertices[0].y+90):
-                                name += symbol.text
-                                if(symbol.property.detected_break.type==1):
-                                    name += ' '
-    #                             if(symbol.property.detected_break.type==2):
-    #                                 name += '\t'
-    #                             if(symbol.property.detected_break.type==3 or symbol.property.detected_break.type==5):
-    #                                 name += '\n'
-                            data['name'] = name
-                            
-                            if(min_x >= bounds[flag].vertices[0].x-20 and max_x <= bounds[flag].vertices[2].x+20 and min_y >= bounds[flag].vertices[0].y+70 and max_y <= bounds[flag].vertices[2].y+20):
-                                address += symbol.text
-                                if(symbol.property.detected_break.type==1):
-                                    address += ' '
-    #                             if(symbol.property.detected_break.type==2):
-    #                                 address += '\t'
-    #                             if(symbol.property.detected_break.type==3 or symbol.property.detected_break.type==5):
-    #                                 address += '\n'
-                            data['address'] = address
-                            
-        return data
+    area_overlap = (max(lw, lc) - min(rw, rc)) * (max(tw, tc) - min(bw, bc))
+    area_word = (rw - lw) * (bw - tw)
 
-    def extract_phone(self):
-        bounds = self.bounds
-        text = ''
-        for page in self.document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            min_x, max_x, min_y, max_y = self.vertices_to_box(symbol.bounding_box.vertices[0],
-                                                                              symbol.bounding_box.vertices[1],
-                                                                              symbol.bounding_box.vertices[2],
-                                                                              symbol.bounding_box.vertices[3])
+    return area_overlap / area_word
 
-                            if(min_x >= bounds[-1].vertices[0].x-20 and max_x <= bounds[-1].vertices[2].x+20 and min_y >= bounds[0].vertices[2].y+20 and max_y <= bounds[-1].vertices[0].y-20):
-                                text += symbol.text
-        return text
+def check_list_overlap(words):
+    if len(words) == 1:
+        return words
 
-    def extract_email(self, flag=-1):
-        bounds = self.bounds
-        text = ''
-        for page in self.document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-                            max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-                            min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-                            max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-                            if(min_x >= bounds[flag].vertices[0].x-20 and max_x <= bounds[flag].vertices[2].x+20 and min_y >= bounds[flag].vertices[0].y-20 and max_y <= bounds[flag].vertices[2].y+20):
-                                text += symbol.text
-        return text
+    boxes = []
+    scores = []
+    for word in words:
+        v = [word.bounding_box.vertices[i] for i in range(4)]
+        boxes.append([v[0].y, v[0].x, v[2].y, v[2].x])
+        scores.append(word.confidence)
+
+    boxes = np.array(boxes)
+    scores = np.array(scores)
+
+    indexes = tf.image.non_max_suppression(boxes, scores, 100, iou_threshold=0.2)
+    indexes = sorted(indexes)
+    words = [words[i] for i in indexes]
+
+    return words
+
+def extract_text_helper(document):
+    p = Cnfg()
+    canvas_pos = p.pos
+    kinds = p.text_kinds
+
+    # Collect words of high overlap with canvas positions of each kind
+    words = {i: [] for i in kinds}
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    v = [word.bounding_box.vertices[i] for i in range(4)]
+                    for kind in kinds:
+                        overlap = intersect(v[0], v[1], v[2], v[3], canvas_pos[kind])
+                        if overlap > 0.5:
+                            words[kind].append(word)
+
+    for kind in kinds:
+        words[kind] = check_list_overlap(words[kind])
+
+    # Collect symbols from each word found
+    text = {i: [] for i in kinds}
+    for kind in kinds:
+        for word in words[kind]:
+            for symbol in word.symbols:
+                if kind == 'phone':
+                    v = [symbol.bounding_box.vertices[i] for i in range(4)]
+                    overlap = intersect(v[0], v[1], v[2], v[3], canvas_pos['left_p'])
+                    if overlap > 0.5:
+                        continue
+                    overlap = intersect(v[0], v[1], v[2], v[3], canvas_pos['right_p'])
+                    if overlap > 0.5:
+                        continue
+
+                text[kind].append(symbol.text)
+                if symbol.property.detected_break.type == 1:
+                    text[kind].append(' ')
+                if symbol.property.detected_break.type in [3, 5]:
+                    text[kind].append('\n')
+
+    # concatenate symbol list
+    for kind in text.keys():
+        text[kind] = ''.join(text[kind])
+
+    address_lines = text['address'].split('\n')
+    if len(address_lines) > 2:
+        text['ID'] = address_lines.pop(0)
+        text['name'] = address_lines.pop(0)
+        text['address'] = ''.join(address_lines)
+    else:
+        raise ValueError("Not enough lines detected in address")
+
+    text['phone'] = ''.join(e for e in text['phone'] if e.isdigit())
+
+    return text
 
 
 if __name__ == '__main__':
-    img_path = r'C:\Users\vinee\source\repos\BlueShield_DocPro\filled_forms\1_filled.png'
-    find_text = FindText(img_path)
-    find_text.extract_phone()
-
-
-
-
+    img_path = r'C:\Users\vinee\source\repos\BlueShield_DocPro\temp\canvas\canvas_18.png'
+    text = find_text(img_path)
+    print(text)
